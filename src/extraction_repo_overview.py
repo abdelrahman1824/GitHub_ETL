05fee958a98c280f4478721be2d60a3e8ad30b15
Hassan <abdelrahman.hassan@capgemini.com>
2026-04-13 19:45:32 +0200

    Splitted the script into functions + Added timeout error handling to fetch_tensoflow_repo func

--- src/extraction_repo_overview.py
+++ src/extraction_repo_overview.py
@@ -1,36 +1,52 @@
 import requests
 from dotenv import load_dotenv
 import os
+import json
 
-# ---------- Loading Environmental Vraiables, Personal Access Token  ---------- #
+# ---------- Configuration ---------- #
 
-#PAT (Personal Access Token) validation
-load_dotenv()
-token = os.getenv("GITHUB_TOKEN")
+def load_github_token (): #PAT (Personal Access Token) validation Function
+    load_dotenv()
 
-if not token:
-    raise ValueError("GITHUB_TOKEN not found in environmental variables")
-else:
-    print("GUTHUB_TOKEN is found!")
+    token = os.getenv("GITHUB_TOKEN")
 
-# ---------- Buildin an API Request from GitHub tensorflow ---------- #
-
-#Building an API request authentication
-url = "https://api.github.com/repos/tensorflow/tensorflow"
+    if not token:
+        raise ValueError("GITHUB_TOKEN not found in environmental variables")
+    else:
+        print("GITHUB_TOKEN is found!")
+    return token
 
 
-headers = {'Authorization': f"bearer {token}", #Authorization head for PAT
+# ---------- Request Setup ---------- #
+
+def build_api_headers(token): #Building an API request authentication Function
+
+    return {'Authorization': f"Bearer {token}", #Authorization head for PAT
            'Accept': 'application/json'} #Accept token for response specification
+    
 
-response = requests.get(url, headers = headers)
+# ---------- Data Extraction ---------- #
 
-#Error handling
-if response.status_code != 200:
-    raise ValueError(f"Error: HTTP {response.status_code}, {response.text}")
-else:
+def fetch_tensorflow_repo(headers):
+    url = "https://api.github.com/repos/tensorflow/tensorflow"
+    connect_timeout = 3
+    read_timeout = 7
+
+    #Error handling
+    try:
+        response = requests.get(url, headers = headers, timeout = (connect_timeout, read_timeout))
+    except requests.exception.Timeout:
+        raise RuntimeError("GitHub API request timeout")
+    
+    if response.status_code != 200:
+        raise ValueError(f"Error: HTTP {response.status_code}, {response.text}")
+
     print(f"Response is successful, HTTP {response.status_code}")
     data = response.json() #Parse JSON data
 
+    return data
+
+token = load_github_token()
+headers = build_api_headers(token)
+data = fetch_tensorflow_repo(headers)
 print(data)
-
-
